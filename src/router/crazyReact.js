3a0190045d684abf7f216ff960980ad2b577ecd0
chickentooth <mr.kurro@gmail.com>
2018-12-05 01:55:49 +0700

    update dist

---
+++ src/router/crazyReact.js
@@ -0,0 +1,10 @@
+import MagicQuote from '../components/MagicQuote'
+
+export default {
+    path: '/magic-quote',
+    name: 'MagicQuote',
+    component: MagicQuote,
+    meta: {
+        requiresAuth : true
+    }
+}
