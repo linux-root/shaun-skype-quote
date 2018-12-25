131410b6b032f20457b0c5dcf0593ac61441cf9a
chickentooth <mr.kurro@gmail.com>
2018-12-26 00:37:36 +0700

    add deployment file

---
+++ update_dist.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "beware that this script only works on Kurro's pc =)))"
+rm -r ././../skypee/dist/
+cp -r ./dist ././../skypee
+echo "dist in path '../../skypee' is updated successfully, deployment can be proceeded"
